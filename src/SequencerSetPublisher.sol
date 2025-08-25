c99323fe9e44c9007ecb3592d5cb1cdf68669c79
eigmax <sd@eigen.market>
2025-08-25 11:10:50 +0800

    chore: simplify code

--- src/SequencerSetPublisher.sol
+++ src/SequencerSetPublisher.sol
@@ -3,58 +3,47 @@ pragma solidity ^0.8.28;
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+
 import "./MultiSigVerifier.sol";
-import "./P2WSHSignatureVerifier.sol";
+import "./interfaces/ISequencerSetPublisher.sol";
 
 // Sequencer Set Publisher
-contract SequencerSetPublisher is Initializable, OwnableUpgradeable {
-    struct SequencerSet {
-        bytes32 sequencer_set_hash; // validator_hash
-        bytes32 publishers_hash; 
-        bytes32 p2wsh_sig_hash;
-        uint256 voting_power;
-        bytes32 next_sequencer_set_hash; // next_validator_hash
-        uint256 goat_block_number;
-    }
-
+contract SequencerSetPublisher is Initializable, OwnableUpgradeable, ISequencerSetPublisher {
+    using ECDSA for bytes32;
+    using MessageHashUtils for bytes32;
+  
     mapping(uint256 height => bytes32[] cmts) public height_cmts;
     mapping(bytes32 cmt => SequencerSet[] ss) public cmt_ss;
 
     uint256 latest_height;
     MultiSigVerifier multiSigVerifier;
-    P2WSHSignatureVerifier p2wshSigVerifier;
 
     function initialize(address initialOwner, address[] calldata initPublishers) public initializer {
         __Ownable_init(initialOwner);
         // ensure valid sigs >= 2/3
         uint quorum = (initPublishers.length * 2 + 1)/3; 
         multiSigVerifier = new MultiSigVerifier(initPublishers, quorum);
-        p2wshSigVerifier = new P2WSHSignatureVerifier();
     }
 
     error P2WSHSignatureMismatch();
     error DoubleCommit();
     error InvalidSequencerSet();
-    error AddressConversionOutOfBounds();
     error MismatchPublisher();
     error InvalidGOATHeight();
     error InvalidVotingPower();
 
     /// @notice Publish a new sequencer set, which should be signed by the older publishers.
     /// @param ss The Sequencer Set 
-    /// @param _v The v-value of the P2WSH signature
-    /// @param _r The r-value of the P2WSH signature
-    /// @param _s The s-value of the P2WSH signature
+    /// @param signature The P2WSH signature
     function updateSequencerSet(
         SequencerSet calldata ss,
-        uint8 _v,
-        bytes32 _r,
-        bytes32 _s
-    ) external {
+        bytes calldata signature
+    ) external override {
         // Check goat block number is valid
         require(ss.goat_block_number >= latest_height, InvalidGOATHeight());
         require(ss.voting_power > 0, InvalidVotingPower());
-        require(p2wshSigVerifier.verifyRSV(ss.p2wsh_sig_hash, msg.sender, _v, _r, _s), P2WSHSignatureMismatch());
+        require(msg.sender == ss.p2wsh_sig_hash.recover(signature), P2WSHSignatureMismatch());
 
         // ensure the publishers is not changed.
         bytes32 expectedPublishersHash = keccak256(
@@ -80,12 +69,16 @@ contract SequencerSetPublisher is Initializable, OwnableUpgradeable {
     // Update publisher at once by multi-sig
     function updatePublisherSet(
         address[] calldata newOwners,
-        bytes[] calldata signatures
-    ) external {
-        bytes32 latest_cmt = calcSequencerSetCmtAtHeightOrLatest(latest_height); 
+        bytes[] calldata signatures,
+        SequencerSet calldata ss,
+        bytes calldata sequencerSetCmtSig
+    ) external override {
+        // if there is no agreement on the latest sequencer set update, it should panic.
+        bytes32 previous_cmt = calcSequencerSetCmtAtHeightOrLatest(latest_height); 
+        this.updateSequencerSet(ss, sequencerSetCmtSig);
         // ensure valid sigs >= 2/3
         uint quorum = (newOwners.length * 2 + 1)/3; 
-        multiSigVerifier.updateOwners(newOwners, quorum, latest_cmt, signatures);
+        multiSigVerifier.updateOwners(newOwners, quorum, previous_cmt, signatures);
     }
 
     // Anyone can calculate the commitment for a given height, and publish it to Bitcoin
---
+++ src/interfaces/ISequencerSetPublisher.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+interface ISequencerSetPublisher {
+    struct SequencerSet {
+        bytes32 sequencer_set_hash; // validator_hash
+        bytes32 publishers_hash; 
+        bytes32 p2wsh_sig_hash;
+        uint256 voting_power;
+        bytes32 next_sequencer_set_hash; // next_validator_hash
+        uint256 goat_block_number;
+    }
+
+    function updateSequencerSet(
+        SequencerSet calldata ss,
+        bytes calldata signature
+    ) external;
+
+     // Update publisher at once by multi-sig
+    function updatePublisherSet(
+        address[] calldata newOwners,
+        bytes[] calldata signatures,
+        SequencerSet calldata ss,
+        bytes calldata sequencerSetCmtSig
+    ) external; 
+}
